5ec970ff7cd8df673c1fe652da3915f495f5965f
Sudhanshu Kalekinge <74017676+26052007@users.noreply.github.com>
2026-06-22 23:37:16 +0530

    Add Lasers [B] from Codeforces

---
+++ 2148/B - Lasers/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    int t;
+    cin>>t;
+    while(t--){
+ 
+        int n,m,x,y;
+        cin>>n>>m>>x>>y;
+ 
+        int a[n];
+        for(int i = 0 ; i < n; i++){
+            cin>>a[i];
+        }
+ 
+        int b[m];
+        for(int i = 0 ; i < m; i++){
+            cin>>b[i];
+        }
+ 
+        int ans = n+m;
+        
+ 
+        cout << ans << endl;
+ 
+    }
+ 
+}
