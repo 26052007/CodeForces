f0b58ae3cd0ce8b09b95837c6db899ca1ca045e1
Sudhanshu Kalekinge <74017676+26052007@users.noreply.github.com>
2026-06-22 23:40:36 +0530

    Add Collatz Conjecture [A] from Codeforces

---
+++ 2137/A - Collatz Conjecture/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    int t;
+    cin>>t;
+    while(t--){
+ 
+        long long k,x;
+        cin>>k>>x;
+ 
+        cout << x*(1LL << k) << endl;
+ 
+ 
+ 
+    }
+ 
+}
