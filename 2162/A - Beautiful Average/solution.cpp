082719f06beb60c966751b01b2be00b4ba5bbe4a
Sudhanshu Kalekinge <74017676+26052007@users.noreply.github.com>
2026-06-22 23:36:29 +0530

    Add Beautiful Average [A] from Codeforces

---
+++ 2162/A - Beautiful Average/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+ 
+    int t;
+    cin>>t;
+    while(t--){
+ 
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i = 0; i < n; i++){
+            cin>>arr[i];
+        }
+ 
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                if(arr[i]>arr[i+1]){
+                    swap(arr[i],arr[i+1]);
+                }
+            }
+        }
+ 
+        cout << arr[n-1] << endl;
+ 
+    }
+ 
+}
