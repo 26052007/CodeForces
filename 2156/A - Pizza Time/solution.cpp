bb12354e1d0b5b290dba6a27802671ac9cbf5c17
Sudhanshu Kalekinge <74017676+26052007@users.noreply.github.com>
2026-06-22 23:38:08 +0530

    Add Pizza Time [A] from Codeforces

---
+++ 2156/A - Pizza Time/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ 
+    int t;
+    cin>>t;
+    while(t--){
+ 
+        int n;
+        cin>>n;
+ 
+        int ans;
+ 
+        if(n%2 != 0){
+            n += 1;
+            ans = (n/2)-1;
+        }
+        else{
+            ans = (n/2)-1;
+        }
+ 
+        cout << ans << endl;
+ 
+    }
+ 
+}
