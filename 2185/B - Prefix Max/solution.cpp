ddca25886b6269be49a756cd1cee8e153ab0276c
Sudhanshu Kalekinge <74017676+26052007@users.noreply.github.com>
2026-06-22 23:35:07 +0530

    Add Prefix Max [B] from Codeforces

---
+++ 2185/B - Prefix Max/solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+ 
+    int t;
+    cin>>t;
+    while(t--){
+ 
+ 
+        int n;
+        cin>>n;
+ 
+        int arr[n];
+        for(int i = 0; i <n;i++){
+            cin>>arr[i];
+        }
+ 
+        int temp;
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                if(arr[j] < arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+ 
+        int max_arr[n];
+        int index = 0;
+        int max = 0;
+ 
+        for(int i = 0; i < n;i++){
+           max+= arr[0];
+        }
+ 
+ 
+ 
+        cout << max << endl;
+ 
+ 
+    }
+ 
+}
