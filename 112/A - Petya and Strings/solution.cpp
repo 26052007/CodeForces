187adf1d95b39acc8faab0eb84714bec3943603c
Sudhanshu Kalekinge <74017676+26052007@users.noreply.github.com>
2026-07-25 00:00:02 +0530

    Add Petya and Strings [A] from Codeforces

---
+++ 112/A - Petya and Strings/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+ 
+using namespace std;
+ 
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+ 
+    for (size_t i = 0; i < s1.length(); i++) {
+        char c1 = tolower(s1[i]);
+        char c2 = tolower(s2[i]);
+ 
+        if (c1 < c2) {
+            cout << -1 << endl;
+            return 0;
+        }
+        if (c1 > c2) {
+            cout << 1 << endl;
+            return 0;
+        }
+    }
+ 
+    cout << 0 << endl;
+    return 0;
+}
