87e05255af7eb4c71908afee9e9e80279caafee4
Sudhanshu Kalekinge <74017676+26052007@users.noreply.github.com>
2026-08-02 00:26:53 +0530

    Add NIT Destroys the Universe [B] from Codeforces

---
+++ 1696/B - NIT Destroys the Universe/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int blocks = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] != 0 && (i == 0 || a[i - 1] == 0)) {
+            blocks++;
+        }
+    }
+    cout << min(2, blocks) << "
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
